62537d8db119da5ca200bb0d6f1360194be0622f
Shagun Gautam <shagungautam1799@gmail.com>
2026-07-02 17:47:37 +0530

    Added back to top button

--- script.js
+++ script.js
@@ -59,3 +59,31 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+// Back To Top Button
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        backToTop.style.display = "block";
+
+    }else{
+
+        backToTop.style.display = "none";
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+        behavior:"smooth"
+
+    });
+
+});
