6117e5786efb78a6de7f1ff0bbf87a17e0729cb6
Shagun Gautam <shagungautam1799@gmail.com>
2026-07-02 17:11:11 +0530

    Added responsive mobile navigation menu

--- script.js
+++ script.js
@@ -51,3 +51,11 @@ counters.forEach(counter => {
     observer.observe(counter);
 
 });
+// Mobile Menu
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
